6aae222b750a3a969b29bf18c2311461c082005f
piwii <cariou.p@gmail.com>
2017-01-22 23:36:15 +0100

    modification des texts

--- js/contact.js
+++ js/contact.js
@@ -1,11 +1,17 @@
 
 $(document).ready(function() {
 
-	function changeContactHref() {
-		$('#ContactSend').attr('href', 'mailto:famille.cariou.escoubas@gmail.com?subject='+ $('#ContactSubject').val() + '&body=' + $('#ContactMessage').val())
-	}
+    function changeContactHref() {
+        $('#ContactSend').attr('href', 'mailto:famille.cariou.escoubas@gmail.com?subject=Mariage Elodie et Pierre-Yves : '+ $('#ContactSubject').val() + '&body=' + $('#ContactMessage').val())
+    }
 
-	$('#ContactSubject').change(changeContactHref);
-	$('#ContactMessage').change(changeContactHref);
+    $('#ContactSubject').change(changeContactHref);
+    $('#ContactMessage').change(changeContactHref);
+    $('#ContactSend').click(function (e) {
+        if ($('#ContactMessage').val() == '') {
+            e.stopPropagation();
+            e.preventDefault();
+        }
+    })
 
 }); // end document ready
